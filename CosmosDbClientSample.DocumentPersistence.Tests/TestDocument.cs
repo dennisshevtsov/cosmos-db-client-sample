ed533f6064b441ab424ad5753d2a00a594f92174
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-02-15 00:19:40 +0300

    add proj

---
+++ CosmosDbClientSample.DocumentPersistence.Tests/TestDocument.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+
+namespace CosmosDbClientSample.DocumentPersistence.Tests
+{
+  using System;
+
+  using CosmosDbClientSample.DocumentPersistence;
+
+  public sealed class TestDocument : DocumentBase
+  {
+    public string StringProperty { get; set; }
+
+    public DateTime DateTimeProperty { get; set; }
+
+    public Guid GuidProperty { get; set; }
+
+    public EmbeddedTestDocument EmbeddedProperty { get; set; }
+  }
+
+  public sealed class EmbeddedTestDocument
+  {
+    public string StringProperty { get; set; }
+
+    public DateTime DateTimeProperty { get; set; }
+
+    public Guid GuidProperty { get; set; }
+  }
+}
---
+++ CosmosDbClientSample.DocumentPersistence/DocumentCollection.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+
+namespace CosmosDbClientSample.DocumentPersistence
+{
+  using System.Collections.Generic;
+
+  /// <summary>Represents detail of a document collection response.</summary>
+  /// <typeparam name="TDocument"></typeparam>
+  public sealed class DocumentCollection<TDocument> where TDocument : DocumentBase
+  {
+    /// <summary>Gets/sets a value that represents an ID of a resource.</summary>
+    public string ResourceId { get; set; }
+
+    /// <summary>Gets/sets a value that represents number of documents.</summary>
+    public int Count { get; set; }
+
+    /// <summary>Gets/sets an object that represents a collection of documents.</summary>
+    public IEnumerable<TDocument> Documents { get; set; }
+  }
+}
